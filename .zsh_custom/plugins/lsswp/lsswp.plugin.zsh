35939bccc2e07a0f8f316f8f09dcd8b45184aae4
Kurt Wilson <kurt4wilson@gmail.com>
2021-03-15 22:11:23 -0400

    add some extra swap-related commands to my lsswp plugin

--- .zsh_custom/plugins/lsswp/lsswp.plugin.zsh
+++ .zsh_custom/plugins/lsswp/lsswp.plugin.zsh
@@ -1,3 +1,11 @@
 function lsswp(){
   for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | head -n $[`tput lines` - 1]
 }
+
+function zswap_ratio(){
+  sudo perl -E  "say $(sudo cat /sys/kernel/debug/zswap/stored_pages) * 4096 / $(sudo cat /sys/kernel/debug/zswap/pool_total_size)"
+}
+
+function zswap_stats(){
+  sudo zsh -c 'cd /sys/kernel/debug/zswap && grep -r .' | sed 's:\::\:	:' | column -t
+}
