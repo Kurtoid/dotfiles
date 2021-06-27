fc64ace14c1261593be7b451472bbd34c7616dda
Kurt Wilson <kurt4wilson@gmail.com>
2021-06-26 23:28:19 -0400

    my new terminal config has a two-line prompt

--- .zsh_custom/plugins/lsswp/lsswp.plugin.zsh
+++ .zsh_custom/plugins/lsswp/lsswp.plugin.zsh
@@ -1,5 +1,5 @@
 function lsswp(){
-  for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | head -n $[`tput lines` - 1]
+  for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | head -n $[`tput lines` - 3]
 }
 
 function zswap_ratio(){
